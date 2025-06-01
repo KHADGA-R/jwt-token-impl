ae0ef06fb48b48ea1b30019f5dd783ec878c2384
KHADGA-R <parajuli.kr@gmail.com>
2025-06-01 13:54:57 -0700

    created JwtUtil class to getUserNameFromToken, getClaimFromToken and getAllChaimsFromToken

--- src/main/java/com/example/jwt/configuration/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/jwt/configuration/JwtAuthenticationEntryPoint.java
@@ -1,20 +1,18 @@
 package com.example.jwt.configuration;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+public class JwtAuthenticationEntryPoint {
 
 
-    @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
--- src/main/java/com/example/jwt/configuration/JwtRequestFilter.java
+++ src/main/java/com/example/jwt/configuration/JwtRequestFilter.java
@@ -2,21 +2,36 @@ package com.example.jwt.configuration;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
 
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException {
 
+      final String header = request.getHeader("Authorization");
+
+      String jwtToken = null;
+      if (header != null && header.startsWith("Bearer ")) {
+        jwtToken = header.substring(7);
+
+        try {
+          // Validate the JWT token
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Unable to get JWT token");
+        } catch (ExpiredJwtException e) {
+            System.out.println("JWT token is expired");
+        }
+
+      }
     }
-
-
 }
